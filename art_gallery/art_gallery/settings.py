81ee91596da82f59354338a464dd8c69371b0636
Strunets Dmitry <d.strunetss@gmail.com>
2024-12-09 22:38:05 +0300

    change settings:)

--- art_gallery/art_gallery/settings.py
+++ art_gallery/art_gallery/settings.py
@@ -38,12 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #rest_framework
     'rest_framework.authtoken',
     'rest_framework',
     'rest_framework_simplejwt',
     #apps
     'user',
-    
     'gallery',
     'picture',
     'comment',
